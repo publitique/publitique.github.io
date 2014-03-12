3d01941b9cdfb8a2a8e0e04b3c342b3fba9374a3
Bertrand Poincloux <bertrand@poincloux.com>
2014-03-12 14:18:37 +0100

    menu-collant

--- js/main.js
+++ js/main.js
@@ -128,7 +128,7 @@ function initializeForm() {
 
 			if (scrollTop > 550 ) {   
 				$('nav, header').addClass('sticky');
-				$('.menu-collant'').slideDown();
+				$('.menu-collant').slideDown();
 				$('header.desktop.sticky, nav.desktop.sticky').stop().animate({
 					top: 15
 				});
