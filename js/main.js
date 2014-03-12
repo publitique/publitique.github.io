16c3bbfc50e6a5ac48601e7cda7cc99c07ff872a
Bertrand Poincloux <bertrand@poincloux.com>
2014-03-12 14:12:46 +0100

    slideout fast

--- js/main.js
+++ js/main.js
@@ -137,7 +137,7 @@ function initializeForm() {
 					top: 0,},
 					1, function() {
 						$('nav, header').removeClass('sticky');
-						$('.sticky-head'); 
+						$('.sticky-head').fadeOut('fast'); 
 				}); 
 			}
 		
